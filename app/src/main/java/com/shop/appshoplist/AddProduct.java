60118bdd514497f9184c2f589915a4ff3f7935e8
UlisesRS1 <rankulimarcus@gmail.com>
2025-04-03 12:33:46 -0600

    Add logic

--- app/src/main/java/com/shop/appshoplist/AddProduct.java
+++ app/src/main/java/com/shop/appshoplist/AddProduct.java
@@ -1,6 +1,10 @@
 package com.shop.appshoplist;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.shop.appshoplist.data.repository.IProductRepository;
+
 public class AddProduct extends AppCompatActivity {
+    private EditText edtProductName;
+    private EditText edtProductPrice;
+    private Button btnAgregar;
+    private IProductRepository iProductRepository;
+    private Intent getBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,6 @@ public class AddProduct extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
--- app/src/main/java/com/shop/appshoplist/UpdateProduct.java
+++ app/src/main/java/com/shop/appshoplist/UpdateProduct.java
@@ -1,6 +1,9 @@
 package com.shop.appshoplist;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.shop.appshoplist.data.model.Product;
+import com.shop.appshoplist.data.repository.IProductRepository;
+import com.shop.appshoplist.data.repository.InMemoryProductRepository;
+
 public class UpdateProduct extends AppCompatActivity {
+
+    private Button btnModificar;
+    private Button btnCancelar;
+    private EditText edtUpdateName;
+    private EditText edtupdatePrice;
+    private Intent getBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,44 @@ public class UpdateProduct extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        runActivity();
     }
+
+    public void runActivity(){
+        init();
+        update();
+        getBackToMainScreen();
+    }
+
+    public void init(){
+        this.btnModificar = findViewById(R.id.btnModificar);
+        this.edtUpdateName = findViewById(R.id.edtModificarProducto);
+        this.edtupdatePrice = findViewById(R.id.edtndModificarValor);
+        this.btnCancelar = findViewById(R.id.btnCancelar);
+
+        this.getBack = new Intent(this, MainActivity.class);
+    }
+
+    public void update(){
+        init();
+
+        int index = getIntent().getIntExtra("index", 0);
+
+        IProductRepository iProductRepository = new InMemoryProductRepository();
+        Product newProduct = new Product();
+
+        this.btnModificar.setOnClickListener(v -> {
+            newProduct.setName(String.valueOf(this.edtUpdateName.getText()));
+            newProduct.setPrice(Double.parseDouble(String.valueOf(this.edtupdatePrice.getText())));
+            newProduct.setChecked(false);
+
+            iProductRepository.modifyProduct(index, newProduct);
+            startActivity(this.getBack);
+        });
+    }
+
+    public void getBackToMainScreen(){
+        this.btnCancelar.setOnClickListener(v -> startActivity(this.getBack));
+    }
+
 }
